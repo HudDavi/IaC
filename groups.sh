73672da8122dfe59061b2036f2246de0384cbf76
Hudson Silva <hudson.davi.cds@gmail.com>
2023-09-20 17:10:50 +0100

    Update Readme

--- groups.sh
+++ groups.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Este script cria diretórios, grupos e usuários no sistema.
+
+# Criação dos diretórios publico, adm, ven e sec
 echo 'Criando os Diretórios: publico, adm, ven, sec'
 
 mkdir /publico
@@ -7,30 +10,35 @@ mkdir /adm
 mkdir /ven
 mkdir /sec
 
+# Criação dos grupos GRP_ADM, GRP_VEN e GRP_SEC
 echo 'Criando os Grupos: GRP_ADM, GRP_VEN, GRP_SEC'
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
+# Criação de usuários no grupo GRP_ADM
 echo 'Criando os Usuários do Grupo: GRP_ADM'
 
 useradd carlos -c 'Carlos' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
 useradd maria -c 'Maria' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
 useradd joao -c 'João' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
 
+# Criação de usuários no grupo GRP_VEN
 echo 'Criando os Usuários do Grupo: GRP_VEN'
 
 useradd debora -c 'Débora' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
 useradd sebastiana -c 'Sebastiana' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
 useradd roberto -c 'Roberto' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
 
+# Criação de usuários no grupo GRP_SEC
 echo 'Criando os Usuários do Grupo: GRP_SEC'
 
 useradd josefina -c 'Josefina' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
 useradd amanda -c 'Amanda' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
 useradd rogerio -c 'Rogério' -m -s /usr/bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
 
+# Definição das permissões dos diretórios
 echo 'Definindo as Permissões dos Diretórios'
 
 chown root:GRP_ADM /adm
@@ -42,4 +50,5 @@ chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
 
+# Indica a conclusão do processo
 echo 'Concluído'
--- users.sh
+++ users.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/bash
 
+# Este script cria quatro usuários no sistema: guest1, guest2, guest3 e guest4.
 echo 'Criando os Usuários: guest1, guest2, guest3, guest4'
 
+# Criação do usuário guest1
 useradd guest1 -c 'Convidado 1' -s /usr/bin/bash -m -p $(openssl passwd -crypt senha123)
 passwd guest1 -e
 
+# Criação do usuário guest2
 useradd guest2 -c 'Convidado 2' -s /usr/bin/bash -m -p $(openssl passwd -crypt senha123)
 passwd guest2 -e
 
+# Criação do usuário guest3
 useradd guest3 -c 'Convidado 3' -s /usr/bin/bash -m -p $(openssl passwd -crypt senha123)
 passwd guest3 -e
 
+# Criação do usuário guest4
 useradd guest4 -c 'Convidado 4' -s /usr/bin/bash -m -p $(openssl passwd -crypt senha123)
 passwd guest4 -e
 
+# Indica a conclusão da criação dos usuários.
 echo 'Concluído'
